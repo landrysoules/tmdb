3c740f4a0b70e4f6c8d99f4fd7184205e481e354
Landry Soules <landry@spaceinvade.rs>
2019-02-03 20:55:09 +0100

    Change window title with movie

--- src/components/movie/TitleBanner.js
+++ src/components/movie/TitleBanner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CircularProgressbar from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import Style from 'style-it';
+import { Helmet } from 'react-helmet';
 
 import './TitleBanner.css';
 
@@ -15,8 +16,12 @@ const TitleBanner = ({ movie, credits }) => {
   };
   if (credits.crew && movie.original_title) {
     return (
-      <Style>
-        {`
+      <>
+        <Helmet>
+          <title>The Movie dB - {movie.original_title}</title>
+        </Helmet>
+        <Style>
+          {`
   .title-banner::before {
     position: absolute;
     content: "";
@@ -32,63 +37,66 @@ const TitleBanner = ({ movie, credits }) => {
     background-size: cover;
 }
     `}
-        <div className="title-banner" style={style}>
-          <div className="section  title-banner-front">
-            <div className="container">
-              <div className="media">
-                <div className="media-left">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${
-                      movie.poster_path
-                    }`}
-                    alt=""
-                  />
-                </div>
-                <div className="media-content movie-content">
-                  <h2 className="title is-2 movie-content">
-                    {movie.original_title} ({movie.releaseYear})
-                  </h2>
-                  <nav className="level">
-                    <div className="level-left">
-                      <div className="level-item">
-                        <div style={{ width: '60px' }}>
-                          <CircularProgressbar
-                            percentage={movie.vote_average * 10}
-                            text={`${movie.vote_average * 10}%`}
-                            background
-                            backgroundPadding={6}
-                          />
+          <div className="title-banner" style={style}>
+            <div className="section  title-banner-front">
+              <div className="container">
+                <div className="media">
+                  <div className="media-left">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${
+                        movie.poster_path
+                      }`}
+                      alt=""
+                    />
+                  </div>
+                  <div className="media-content movie-content">
+                    <h2 className="title is-2 movie-content">
+                      {movie.original_title} ({movie.releaseYear})
+                    </h2>
+                    <nav className="level">
+                      <div className="level-left">
+                        <div className="level-item">
+                          <div style={{ width: '60px' }}>
+                            <CircularProgressbar
+                              percentage={movie.vote_average * 10}
+                              text={`${movie.vote_average * 10}%`}
+                              background
+                              backgroundPadding={6}
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div>User</div>
+                          <br />
+                          <div>score</div>
                         </div>
                       </div>
-                      <div>
-                        <div>User</div>
-                        <br />
-                        <div>score</div>
-                      </div>
-                    </div>
-                    <div className="level-item has-text-centered">
-                      <div>
-                        {/* <p class="heading">Tweets</p>
+                      <div className="level-item has-text-centered">
+                        <div>
+                          {/* <p class="heading">Tweets</p>
                         <p class="title">3,456</p> */}
+                        </div>
                       </div>
-                    </div>
-                  </nav>
-                  <div>
-                    <h5 className="title is-5 movie-content">Overview</h5>
+                    </nav>
+                    <div>
+                      <h5 className="title is-5 movie-content">Overview</h5>
 
-                    <p>{movie.overview}</p>
-                  </div>
-                  <div>
-                    <h5 className="title is-5 movie-content">Featured crew</h5>
-                    <p>{credits.crew[0].name}</p>
-                    <p>{credits.crew[0].job}</p>
+                      <p>{movie.overview}</p>
+                    </div>
+                    <div>
+                      <h5 className="title is-5 movie-content">
+                        Featured crew
+                      </h5>
+                      <p>{credits.crew[0].name}</p>
+                      <p>{credits.crew[0].job}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </Style>
+        </Style>
+      </>
     );
   }
   return null;
