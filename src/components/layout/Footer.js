ede6c3d7574f0d454386c705b9a79f02bbdd914b
Landry Soules <landry@spaceinvade.rs>
2019-02-10 17:23:58 +0100

    WIP Implement footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import './Footer.css';
+import logo from './tmdb-logo.svg';
 
-const Footer = () => <div>Footer</div>;
+const Footer = () => (
+    <footer className="footer bottom-background">
+        <div className="content">
+            <div className="columns">
+                <div className="column is-one-quarter" />
+                <div className="column">
+                    <img id="logo" src={logo} alt="" style={{ marginTop: '5em' }} />
+                </div>
+            </div>
+        </div>
+    </footer>
+);
 
 export default Footer;
