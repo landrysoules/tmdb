22bf9ef5c2074741e7c1e86e982861ac42ccca56
Landry Soules <landry@spaceinvade.rs>
2019-02-23 12:38:31 +0100

    Make container width of 960px

--- src/components/cast/top-billed-cast/CastCard.js
+++ src/components/cast/top-billed-cast/CastCard.js
@@ -21,14 +21,6 @@ const CastCard = ({ cast }) => {
             <p>{cast.character}</p>
           </div>
         </div>
-
-        {/* <div className="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-          iaculis mauris. <a>@bulmaio</a>.<a href="#">#css</a>{' '}
-          <a href="#">#responsive</a>
-          <br />
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div> */}
       </div>
     </div>
   );
