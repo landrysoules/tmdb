46005717a074fc6ae707b056ae199d1494b6923c
Landry Soules <landry@spaceinvade.rs>
2019-02-23 16:00:58 +0100

    Improve KnownForCard display
    - Modify the style to look like tmdb
    - Increase $desktop up to 1000px, like tmdb

--- src/components/person/KnownFor.js
+++ src/components/person/KnownFor.js
@@ -4,12 +4,17 @@ import KnownForCard from './KnownForCard';
 const KnownFor = ({ credits }) => {
   const cards = credits.map(credit => {
     return (
-      <div className="column">
+      <div className="column" style={{ paddingRight: '0px' }}>
         <KnownForCard credit={credit} />
       </div>
     );
   });
-  return <div className="columns">{cards}</div>;
+  return (
+    <>
+      <h4 class="title is-4">Known for</h4>
+      <div className="columns is-multiline">{cards}</div>
+    </>
+  );
 };
 
 export default KnownFor;
--- src/components/person/KnownForCard.js
+++ src/components/person/KnownForCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { IMAGE_SIZE, URL } from '../../constants';
+import './KnownForCard.css';
 
 const KnownForCard = ({ credit }) => {
   return (
     <>
-      <figure className="image" height="225px" width="150px">
+      <figure className="image">
         <img
+          className="card-image"
           alt={credit.original_title}
           src={`${URL.IMAGES}${IMAGE_SIZE.THUMB}/${credit.poster_path}`}
         />
